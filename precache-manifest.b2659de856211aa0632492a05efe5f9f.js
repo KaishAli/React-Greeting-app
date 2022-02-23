d0a4322194264ef946e5fbf3e42e63544f97ee48
KaishAli <360mohdkaishali.cs@gmail.com>
2022-02-24 02:09:07 +0530

    Updates

---
+++ precache-manifest.b2659de856211aa0632492a05efe5f9f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e6ec82e848e45a9e24ee",
+    "url": "/React-Greeting-app/static/css/main.93956875.chunk.css"
+  },
+  {
+    "revision": "e6ec82e848e45a9e24ee",
+    "url": "/React-Greeting-app/static/js/main.e6ec82e8.chunk.js"
+  },
+  {
+    "revision": "43ce605ecad8a4ab63dc",
+    "url": "/React-Greeting-app/static/js/1.43ce605e.chunk.js"
+  },
+  {
+    "revision": "50f0de1ff6ae253788d9",
+    "url": "/React-Greeting-app/static/js/runtime~main.50f0de1f.js"
+  },
+  {
+    "revision": "535481f91a92b480f191b668e7e4e409",
+    "url": "/React-Greeting-app/index.html"
+  }
+];
